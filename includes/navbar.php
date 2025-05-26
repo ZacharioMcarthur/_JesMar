182b70ef706f8d0171aff81518614a6f3a04f458
ZacharioMcarthur <nascimentozachario@gmail.com>
2025-05-26 02:41:43 +0100

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="home.php">JesMar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="home.php">Accueil</a></li>
+        <li class="nav-item"><a class="nav-link" href="formation-and-services.php">Formations</a></li>
+        <li class="nav-item"><a class="nav-link" href="events.php">Évènements</a></li>
+        <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
